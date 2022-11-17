d681620d1c06a0fa0b4d65ed64174442ba15cc6f
aerochippie <olgszn82433@stud.noroff.no>
2022-11-17 06:13:56 +0100

    updates to webpack again

--- cypress/e2e/returnvalidtoken.cy.js
+++ cypress/e2e/returnvalidtoken.cy.js
@@ -3,5 +3,14 @@
 describe("empty spec", () => {
   it("passes", () => {
     cy.visit("https://aerochippie.github.io/social-media-client/");
+    cy.contains("Login").click();
+    cy.get("#loginEmail")
+      .type("dummytest33@stud.noroff.no")
+      .should("have.value", "dummytest33@stud.noroff.no");
+    cy.get("#loginPassword")
+      .type("dummytest123")
+      .should("have.value", "dummytest123");
+
+    cy.get("#loginForm button").contains("Login").click();
   });
 });
